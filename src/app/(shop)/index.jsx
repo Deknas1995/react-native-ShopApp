1cd9aa783c495b5d995d56b78058cc4d2af6df5d
Armin <armin.raaaaad@gmail.com>
2024-11-11 21:29:22 +0100

    first commit

---
+++ src/app/(shop)/index.jsx
@@ -0,0 +1,45 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { PRODUCTS } from "../../../assets/products";
+import React from "react";
+import { ProductListItem } from "../components/product-list-item";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => (
+          <ProductListItem product={item} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.FlatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  FlatListContent:{
+    paddingBottom: 20,
+  },
+  flatListColumn:{
+    justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  itemContainer: {
+    padding: 16,
+    backgroundColor: "#f9c2ff",
+    marginBottom: 8,
+    borderRadius: 8,
+  },
+  itemText: {
+    fontSize: 18,
+  },
+});
---
+++ src/app/product/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+
+export default function Product(){
+    return(
+        <View>
+            <Text>Product</Text>
+        </View>
+    );
+}
